d251952fc2356b37affda9f1e61ace7c68e3394d
Vladi96 <vladi_9_6@abv.bg>
2018-03-14 20:54:32 +0200

    added Problem 2

---
+++ intro-and-basic-syntax/exercise/Rectangle Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P02_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double widht = double.Parse(Console.ReadLine());
+            double hight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{widht*hight:f2}");
+        }
+    }
+}
