1ad317fd97dc08f05d15e431adf3654338dcaa5d
Georgilqlq <lqlqgeorgi@abv.bg>
2018-03-14 20:42:06 +0200

    Adding a solution for Problem 1: Debit Card Number

---
+++ intro-and-basic-syntax/exercise/Debit Card Number/Problem 1 Debit Card Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1_Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(number.ToString("D4") + " " + number1.ToString("D4") + " " + number2.ToString("D4") + " " + number3.ToString("D4"));
+
+        }
+    }
+}
