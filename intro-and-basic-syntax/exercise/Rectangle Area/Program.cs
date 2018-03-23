ea35b8b5f8b3750a8a309e5bea9208fb25952cc1
Vladi96 <vladi_9_6@abv.bg>
2018-03-23 19:31:21 +0200

    Documents Projekt 03

--- intro-and-basic-syntax/exercise/Rectangle Area/Program.cs
+++ intro-and-basic-syntax/exercise/Rectangle Area/Program.cs
@@ -6,9 +6,11 @@ namespace P02_Rectangle_Area
     {
         static void Main(string[] args)
         {
+            //Take widht from Console
             double widht = double.Parse(Console.ReadLine());
+            //Take hight from Console
             double hight = double.Parse(Console.ReadLine());
-
+            //Print in Console
             Console.WriteLine($"{widht*hight:f2}");
         }
     }
